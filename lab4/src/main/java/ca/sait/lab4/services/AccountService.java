b1f3f31998004ae6ebc1b798f8efc724410b6432
CapBarbosssa <97461923+CapBarbosssa@users.noreply.github.com>
2022-02-06 15:25:44 -0700

    AccountService added

---
+++ lab4/src/main/java/ca/sait/lab4/services/AccountService.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sait.lab4.services;
+
+import ca.sait.lab4.models.User;
+
+/**
+ *
+ * @author peony
+ */
+public class AccountService {
+    
+    public User login(String username, String password){
+        
+        if(username.equals("abe")&& password.equals("password")){
+            
+        }
+        else if (username.equals("barb")&& password.equals("password")){
+            
+        }
+        else{
+            
+        }
+        return null;
+        
+    }
+}
